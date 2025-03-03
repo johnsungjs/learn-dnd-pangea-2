01cd40966a0d4d263c446e5792d1c38db572fba4
johnsungjs <johnsungjs@gmail.com>
2025-03-03 09:52:45 +0700

    success drag drop but shringking

---
+++ src/util/initialData.ts
@@ -0,0 +1,14 @@
+export const initialWebComponents = [
+  {
+    id: "compo-1",
+    title: "Jumbotron",
+  },
+  {
+    id: "compo-2",
+    title: "Text",
+  },
+  {
+    id: "compo-3",
+    title: "Footer",
+  },
+];
